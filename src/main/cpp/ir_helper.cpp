c8b99aadd3bc4caf35878e20443560d4022601e6
Ethan Bierlein <ebr4d@live.com>
2019-01-26 11:43:19 -0600

    Update ir_helper.cpp

--- src/main/cpp/ir_helper.cpp
+++ src/main/cpp/ir_helper.cpp
@@ -1,65 +1,36 @@
-//*****************************************************************************
-// Filename:            ir_helper.cpp
-//
-// Revision Record:
-//   Author             Date       Description
-//   ------------------ ---------- --------------------------------------------
-//   Chris Struck       Feb. 2018  Initial design.
-//
-// Description:
-//    This class operates a variety of infrared sensors, at varying distances.
-// 
-// Dependencies:
-//    None
-//*****************************************************************************
-
-/******************************************************************************
- * Include Files
- *****************************************************************************/
-// System Includes //
 #include <iostream>
-
-// First Includes //
 #include <frc/AnalogInput.h>
-
-// Our Includes //
 #include "ir_helper.hpp"
 
-/*-----------------------------------------------------------------------------
- * FUNCTION NAME:    Infrared
- *---------------------------------------------------------------------------*/
 Infrared::Infrared(int irChannel, Infrared_Sensor_Type irType)
 {
-    irSensor = new frc::AnalogInput(irChannel);
-    sensorFamily = irType;
+    	irSensor = new frc::AnalogInput(irChannel);
+    	sensorFamily = irType;
     
-    switch(sensorFamily)
+    	switch(sensorFamily)
 	{
-	case GP2Y0A710K0F:			//3-18 feet
-		VOLTAGE_SCALING = 137.5;
-		VOLTAGE_INTERCEPT = 1.125;
-		break;
+		case GP2Y0A710K0F:			// 3-18 feet
+			VOLTAGE_SCALING = 137.5;
+			VOLTAGE_INTERCEPT = 1.125;
+			break;
 
-	case GP2Y0A02YK0F:			//7-59 inches
-		VOLTAGE_SCALING = 46.5909091;
-		VOLTAGE_INTERCEPT = 0.33522727272;
-		break;
+		case GP2Y0A02YK0F:			// 7-59 inches
+			VOLTAGE_SCALING = 46.5909091;
+			VOLTAGE_INTERCEPT = 0.33522727272;
+			break;
 
-	case OPB732WZ:				// up to 3 inches
-		VOLTAGE_SCALING = 1.0;
-		VOLTAGE_INTERCEPT = 1.0;
-		break;
+		case OPB732WZ:				// up to 3 inches
+			VOLTAGE_SCALING = 1.0;
+			VOLTAGE_INTERCEPT = 1.0;
+			break;
 
-	default:
-		VOLTAGE_SCALING = 1.0;
-		VOLTAGE_INTERCEPT = 1.0;
-		break;
+		default:
+			VOLTAGE_SCALING = 1.0;
+			VOLTAGE_INTERCEPT = 1.0;
+			break;
 	}
 }
 
-/*-----------------------------------------------------------------------------
- * FUNCTION NAME:    checkColor
- *---------------------------------------------------------------------------*/
 Color Infrared::checkColor()
 {
 	double irValue = Voltage();
@@ -81,20 +52,13 @@ Color Infrared::checkColor()
 	return rtnColor;
 }
 
-/*-----------------------------------------------------------------------------
- * FUNCTION NAME:    irRange
- *---------------------------------------------------------------------------*/
 double Infrared::irRange()
 {
-	//Gets the ir sensor voltage and converts it into inches
 	double rangeInches = 0.0;
 	rangeInches = 1 / ( ( irSensor->GetVoltage() - VOLTAGE_INTERCEPT ) / ( VOLTAGE_SCALING * 0.393701) );
 	return rangeInches;
 }
 
-/*-----------------------------------------------------------------------------
- * FUNCTION NAME:    Voltage
- *---------------------------------------------------------------------------*/
 double Infrared::Voltage()
 {
 	double volt = 0;
@@ -102,9 +66,6 @@ double Infrared::Voltage()
 	return volt;
 }
 
-/*-----------------------------------------------------------------------------
- * FUNCTION NAME:    AverageRaw
- *---------------------------------------------------------------------------*/
 int Infrared::AverageRaw()
 {
 	double volt = 0;
