2a3227e94de836df35bf034b89dcaa52bacd5d4c
Ethan Bierlein <ebr4d@live.com>
2019-01-31 19:53:34 -0600

    Add some more basic processing utilities.

--- src/main/include/vision_processing.hpp
+++ src/main/include/vision_processing.hpp
@@ -1,6 +1,8 @@
 #ifndef LIMELIGHT_CAMERA_HELPER_HPP
 #define LIMELIGHT_CAMERA_HELPER_HPP
 
+#include <memory>
+#include <memory.h>
 #include <frc/smartdashboard/Smartdashboard.h>
 #include <networktables/NetworkTable.h>
 
@@ -12,9 +14,29 @@
 class VisionProcessing
 {
 public:
+    /**
+     * @constructor VisionProcessing
+     * @description Create an instance of the VisionProcessing class.
+     */
     VisionProcessing();
 
+    /**
+     * @function
+     * @description Update the set of Limelight camera properties used.
+     * @notes       The properties updated are:
+     *                  tv, tx, ty, ta, ts, thor, tvert
+     */
+    void UpdateLimelightProperties();
+
 private:
+    std::shared_ptr<NetworkTable> networkTable;
+    bool limelightTargets_TV;
+    double limelightHorizontalOffset_TX;
+    double limelightVerticalOffset_TY;
+    double limelightTargetArea_TA;
+    double limelightSkew_TS;
+    double limelightBoundingBoxWidth_THOR;
+    double limelightBoundingBoxHeight_TVERT;
 };
 
 #endif
