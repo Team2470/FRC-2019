b41ab591c17e5a18b6bd45a68c659066fa125899
PLTW User <pltw@bloomington.k12.mn.us>
2019-01-31 19:26:01 -0600

    Added basic vision processing framework.

---
+++ src/main/cpp/vision_processing.cpp
@@ -0,0 +1,1 @@
+#include "vision_processing.hpp"
---
+++ src/main/include/vision_processing.hpp
@@ -0,0 +1,20 @@
+#ifndef LIMELIGHT_CAMERA_HELPER_HPP
+#define LIMELIGHT_CAMERA_HELPER_HPP
+
+#include <frc/smartdashboard/Smartdashboard.h>
+#include <networktables/NetworkTable.h>
+
+/**
+ * @class       VisionProcessing
+ * @description This class is responsible for all vision processing, powered
+ *              through the Limelight 2.0 camera.
+ */
+class VisionProcessing
+{
+public:
+    VisionProcessing();
+
+private:
+};
+
+#endif
