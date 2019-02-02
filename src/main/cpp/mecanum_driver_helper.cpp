03543c5d0a80c6c95d27dffd29a162508f251d26
Ethan Bierlein <ebr4d@live.com>
2019-02-02 09:26:45 -0600

    Minor formatting changes to the mecanum drive implementation in preparation for new features.

--- src/main/cpp/mecanum_driver_helper.cpp
+++ src/main/cpp/mecanum_driver_helper.cpp
@@ -7,89 +7,95 @@
 #include "mecanum_driver_helper.hpp"
 
 BjorgMecanumDrive::BjorgMecanumDrive(
-        frc::Spark* m_frontleftMotor, 
-        frc::Spark* m_backleftMotor, 
-        frc::Spark* m_frontrightMotor, 
-        frc::Spark* m_backrightMotor,
-        frc::Joystick* moveController, 
-        frc::Joystick* shiftController, 
-        frc::Joystick* rotateController
+    frc::Spark* m_frontleftMotor, 
+    frc::Spark* m_backleftMotor, 
+    frc::Spark* m_frontrightMotor, 
+    frc::Spark* m_backrightMotor,
+    frc::Joystick* moveController, 
+    frc::Joystick* shiftController, 
+    frc::Joystick* rotateController
 )
 {
-        m_robotDrive = new frc::MecanumDrive { *m_frontleftMotor, *m_backleftMotor, *m_frontrightMotor, *m_backrightMotor };
-        driveControllerMove = moveController;
-        driveControllerShift = shiftController;
-        driveControllerRotate = rotateController;
-        m_robotDrive->SetExpiration(0.1);
+    m_robotDrive = new frc::MecanumDrive 
+    { 
+        *m_frontleftMotor, 
+        *m_backleftMotor, 
+        *m_frontrightMotor, 
+        *m_backrightMotor 
+    };
+    
+    driveControllerMove = moveController;
+    driveControllerShift = shiftController;
+    driveControllerRotate = rotateController;
+    m_robotDrive->SetExpiration(0.1);
 }
 
 void BjorgMecanumDrive::mecanumDrive()
 {
-        m_robotDrive->SetSafetyEnabled(true);
-        setMovement();
-        m_robotDrive->DriveCartesian(moveMultiplier * movementValue, shiftMultiplier * shiftValue, rotateMultiplier * rotateValue);
+    m_robotDrive->SetSafetyEnabled(true);
+    setMovement();
+    m_robotDrive->DriveCartesian(moveMultiplier * movementValue, shiftMultiplier * shiftValue, rotateMultiplier * rotateValue);
 }
 
 void BjorgMecanumDrive::mecanumDrive(double movement, double shift, double rotate)
 {
-        m_robotDrive->SetSafetyEnabled(true);
-        m_robotDrive->DriveCartesian(movement, shift, rotate);
+    m_robotDrive->SetSafetyEnabled(true);
+    m_robotDrive->DriveCartesian(movement, shift, rotate);
 }
 
 void BjorgMecanumDrive::twoBtnMove()
 {
-        movementValue = driveControllerMove->GetRawAxis(fwdMove) - driveControllerMove->GetRawAxis(bckMove);
+    movementValue = driveControllerMove->GetRawAxis(fwdMove) - driveControllerMove->GetRawAxis(bckMove);
 }
 
 void BjorgMecanumDrive::twoBtnShift()
 {
-        shiftValue = driveControllerShift->GetRawAxis(fwdShift) - driveControllerShift->GetRawAxis(bckShift);
+    shiftValue = driveControllerShift->GetRawAxis(fwdShift) - driveControllerShift->GetRawAxis(bckShift);
 }
 
 void BjorgMecanumDrive::twoBtnRotate()
 {
-        rotateValue = driveControllerRotate->GetRawAxis(fwdRotate) - driveControllerRotate->GetRawAxis(bckRotate);
+    rotateValue = driveControllerRotate->GetRawAxis(fwdRotate) - driveControllerRotate->GetRawAxis(bckRotate);
 }
     
 void BjorgMecanumDrive::setMovement()
 {
-        if (multiMove)
-        {
-                twoBtnMove();
-        }
-        else
-        {
-                movementValue = driveControllerMove->GetRawAxis(moveCtrl);
-        }
+    if(multiMove)
+    {
+        twoBtnMove();
+    }
+    else
+    {
+        movementValue = driveControllerMove->GetRawAxis(moveCtrl);
+    }
 
-        if (multiShift)
-        {
-                twoBtnShift();
-        }
-        else
-        {
-                shiftValue = driveControllerShift->GetRawAxis(shiftCtrl);
-        }
+    if(multiShift)
+    {
+        twoBtnShift();
+    }
+    else
+    {
+        shiftValue = driveControllerShift->GetRawAxis(shiftCtrl);
+    }
 
-        if (multiRotate)
-        {
-                twoBtnRotate();
-        }
-        else
-        {
-                rotateValue = driveControllerRotate->GetRawAxis(rotateCtrl);
-        }
+    if(multiRotate)
+    {
+        twoBtnRotate();
+    }
+    else
+    {
+        rotateValue = driveControllerRotate->GetRawAxis(rotateCtrl);
+    }
 }
 
 void BjorgMecanumDrive::setRotate(bool rotateEn)
 {
-        if (multiRotate)
-        {
-                // twoBtnRotate();
-        }
-
-        else
-        {
-                // rotationValue = (rotateEn ? driveController2->GetRawAxis(rotateCtrl) : 0);
-        }
+    if(multiRotate)
+    {
+        //twoBtnRotate();
+    }
+    else
+    {
+        //rotationValue = (rotateEn ? driveController2->GetRawAxis(rotateCtrl) : 0);
+    }
 }
--- src/main/include/mecanum_driver_helper.hpp
+++ src/main/include/mecanum_driver_helper.hpp
@@ -6,21 +6,21 @@
 #include <frc/Spark.h>
 
 /**
- * @class	BjorgMecanumDrive
+ * @class		BjorgMecanumDrive
  * @description This class operates a SpeedController group using the mecanum drive;
- 		it has the ability to control the mecanum drive.
+ *				it has the ability to control the mecanum drive.
  */
 class BjorgMecanumDrive 
 {
 public:
 	int moveCtrl = 0;
-    	int shiftCtrl = 0;
+    int shiftCtrl = 0;
 	int rotateCtrl = 0;
 	//int rtTurn = 0;
 	//int lftTurn = 0;
 	int fwdMove = 0;
 	int bckMove = 0;
-    	int fwdShift = 0;
+    int fwdShift = 0;
 	int bckShift = 0;
  	int fwdRotate = 0;
 	int bckRotate = 0;
@@ -29,8 +29,8 @@ public:
 	//bool rotateEnable = true;
 	//bool sqrInput = false;
 	bool multiMove   = false;
-    	bool multiShift  = false;
-    	bool multiRotate = false;
+	bool multiShift  = false;
+	bool multiRotate = false;
 	float moveMultiplier = 1.0;
  	float shiftMultiplier = 1.0;
 	float rotateMultiplier = 1.0;
@@ -38,20 +38,20 @@ public:
 	/**
 	 * @constructor BjorgMecanumDrive
 	 * @description Construct the BjorgMecanumDrive.
-	 * @param	m_frontleftMotor  -- The front left motor of the mecanum drive.
-	 * @param	m_backleftMotor   -- The back left motor of the mecanum drive.
-	 * @param	m_frontrightMotor -- The front right motor of mecanum drive.
-	 * @param	m_backrightMotor  -- The back right motor of the mecanum drive.
-	 * @param	moveController    -- The joystick controlling forward-backward translational movement.
-	 * @param 	shiftController   -- The joystick controlling left-right translational movement.
-	 * @param	rotateController  -- The joystick controlling rotational movement.
+	 * @param		m_frontleftMotor  -- The front left motor of the mecanum drive.
+	 * @param		m_backleftMotor   -- The back left motor of the mecanum drive.
+	 * @param		m_frontrightMotor -- The front right motor of mecanum drive.
+	 * @param		m_backrightMotor  -- The back right motor of the mecanum drive.
+	 * @param		moveController    -- The joystick controlling forward-backward translational movement.
+	 * @param 		shiftController   -- The joystick controlling left-right translational movement.
+	 * @param		rotateController  -- The joystick controlling rotational movement.
 	 */
 	BjorgMecanumDrive(
 		frc::Spark* m_frontleftMotor, 
 		frc::Spark* m_backleftMotor, 
 		frc::Spark* m_frontrightMotor, 
 		frc::Spark* m_backrightMotor,
-    		frc::Joystick* moveController, 
+    	frc::Joystick* moveController, 
 		frc::Joystick* shiftController, 
 		frc::Joystick* rotateController
 	);
@@ -59,17 +59,17 @@ public:
 	/**
 	 * @function	mecanumDrive
 	 * @description Controls the mecanum drive with the supplied joysticks; it governs all
-	 *		movement, translational and rotational.
+	 *				movement, translational and rotational.
 	 */
 	void mecanumDrive();
 
 	/**
 	 * @function
 	 * @description Controls the mecanum drive with explicitly supplied values; it governs
-	 * 		all movement, translational and rotational.
-	 * @param	movement -- Forward-backward translation velocity.
-	 * @param	shift    -- Left-right translational velocity.
-	 * @param	rotate   -- Rotational velocity.
+	 * 				all movement, translational and rotational.
+	 * @param		movement -- Forward-backward translation velocity.
+	 * @param		shift    -- Left-right translational velocity.
+	 * @param		rotate   -- Rotational velocity.
 	 */
 	void mecanumDrive(double movement, double shift, double rotate);
 
@@ -93,12 +93,12 @@ public:
 
 private:
 	float movementValue = 0.0;
-    	float shiftValue = 0.0;
+    float shiftValue = 0.0;
 	float rotateValue = 0.0;
 	frc::MecanumDrive* m_robotDrive;
 	frc::Joystick* driveControllerMove;
 	frc::Joystick* driveControllerShift;
-    	frc::Joystick* driveControllerRotate;
+    frc::Joystick* driveControllerRotate;
 	
 	/**
 	 * @function	setMovement
