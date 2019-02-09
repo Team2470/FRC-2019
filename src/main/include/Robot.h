1ea405d0b6744af12c90230da5a8825e56a2a6bc
Ethan Bierlein <ebr4d@live.com>
2019-02-09 10:59:35 -0600

    Add comment.

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -22,15 +22,9 @@
 #include "ir_helper.hpp"
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled by the switches on the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use TimedRobot or Command-Based
- * instead if you're new.
+ * @class       Robot
+ * @description Responsible for controlling the robot at the top-level.
+ * @notes       Extends frc::SampleRobot.
  */
 class Robot : public frc::SampleRobot 
 {
